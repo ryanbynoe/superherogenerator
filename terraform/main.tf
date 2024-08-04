ac2a5a271811f18c6ed3a2153692338f654c9dc2
Ryan Bynoe <73267001+ryanbynoe@users.noreply.github.com>
2024-08-04 13:33:13 -0400

    Updates to .gitignore and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,27 +51,6 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
 }
 
-resource "aws_eks_node_group" "eks_nodes" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "superherogen-nodes" # Node group name
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = aws_subnet.eks_subnet[*].id
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-
-  instance_types = ["t3.medium"] # Instance type for the nodes
-
-  # Specify the AMI ID for the node group
-  launch_template {
-    id      = aws_launch_template.eks_node_launch_template.id
-    version = "$Latest"
-  }
-}
-
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role" # IAM role name for nodes
 
@@ -104,23 +83,20 @@ resource "aws_iam_role_policy_attachment" "eks_registry_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_launch_template" "eks_node_launch_template" {
-  name_prefix   = "eks-node-"
-  image_id       = "ami-0a31f06d64a91614b" # Amazon Linux 2 AMI ID
-  instance_type  = "t3.medium"
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "superherogen-nodes" # Node group name
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.eks_subnet[*].id
 
-  lifecycle {
-    create_before_destroy = true
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
-  # Define the IAM instance profile for the nodes
-  iam_instance_profile {
-    name = aws_iam_instance_profile.eks_node_instance_profile.name
+  launch_template {
+    id      = "lt-0e738e8f6a66ba511" # Your existing launch template ID
+    version = "$Latest"              # Use the latest version of the launch template
   }
-}
-
-resource "aws_iam_instance_profile" "eks_node_instance_profile" {
-  name = "eks-node-instance-profile"
-
-  role = aws_iam_role.eks_node_role.name
 }
