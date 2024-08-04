d207198c02558d0326c7df6ada1a2b68bfd1a219
Ryan Bynoe <73267001+ryanbynoe@users.noreply.github.com>
2024-08-04 12:57:06 -0400

    Updates to .gitignore and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,5 @@
 provider "aws" {
-  region = "us-east-2" # AWS region where the EKS cluster will be created
-}
-
-data "aws_availability_zones" "available" {
-  state = "available" # Fetch the available availability zones
+  region = "us-east-2" # AWS region
 }
 
 resource "aws_vpc" "eks_vpc" {
@@ -11,37 +7,93 @@ resource "aws_vpc" "eks_vpc" {
 }
 
 resource "aws_subnet" "eks_subnet" {
-  count                   = 2
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
-  map_public_ip_on_launch = true
+  count             = 2
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 }
 
-data "aws_iam_role" "eks_role" {
-  name = "eks-service-role" # IAM role for EKS
-}
+data "aws_availability_zones" "available" {}
 
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "superherogen-cluster" # Name of the EKS cluster
-  role_arn = data.aws_iam_role.eks_role.arn
+  name     = "superherogen-cluster" # Cluster name
+  role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
     subnet_ids = aws_subnet.eks_subnet[*].id
   }
 }
 
+resource "aws_iam_role" "eks_role" {
+  name = "eks-role" # IAM role name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "eks-nodes" # Name of the EKS node group
-  node_role_arn   = data.aws_iam_role.eks_role.arn
+  node_group_name = "superherogen-nodes" # Node group name
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.eks_subnet[*].id
 
   scaling_config {
-    desired_size = 2 # Desired number of nodes
-    max_size     = 3 # Maximum number of nodes
-    min_size     = 1 # Minimum number of nodes
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
-  instance_types = "t3.medium" # Instance type for the nodes
+  instance_types = ["t3.medium"] # Instance type for the nodes
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role" # IAM role name for nodes
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_registry_policy_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
