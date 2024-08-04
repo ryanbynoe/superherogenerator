2643afe0d19f61ce3fd29aa0b9fed16f7f67e801
Ryan Bynoe <73267001+ryanbynoe@users.noreply.github.com>
2024-08-04 15:02:23 -0400

    Updates to .gitignore and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,7 +112,7 @@ resource "aws_eks_node_group" "superherogen-nodes" {
   }
 
   launch_template {
-    id      = "lt-0e738e8f6a66ba511" # Existing launch template ID
+    id      = "lt-029da6ff0842ef8bd" # Existing launch template ID
     version = "$Latest"
   }
 }
