0f100dbb0eec1a2cabfb9849511009cf1dc9e301
Ryan Bynoe <73267001+ryanbynoe@users.noreply.github.com>
2024-08-04 15:01:07 -0400

    Updates to .gitignore and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,27 +2,67 @@ provider "aws" {
   region = "us-east-2" # AWS region
 }
 
+# VPC
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16" # CIDR block for the VPC
 }
 
-resource "aws_subnet" "eks_subnet" {
-  count             = 2
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24" # Public subnet CIDR block
+  availability_zone       = element(data.aws_availability_zones.available.names, 0)
+  map_public_ip_on_launch = true
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24" # Private subnet CIDR block
+  availability_zone       = element(data.aws_availability_zones.available.names, 1)
 }
 
 data "aws_availability_zones" "available" {}
 
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+}
+
+# Route Table for Private Subnet
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
+
+# Associate Route Table with Private Subnet
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# EKS Cluster and Node Group setup
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "superherogen-cluster" # Cluster name
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.eks_subnet[*].id
+    subnet_ids = [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
   }
 }
+
+# IAM Roles and Node Groups
+# ... (Include your existing IAM role, policy attachments, and node group configuration here) ...
 
 resource "aws_iam_role" "eks_role" {
   name = "eks-role" # IAM role name
@@ -39,16 +79,6 @@ resource "aws_iam_role" "eks_role" {
       },
     ],
   })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
 }
 
 resource "aws_iam_role" "eks_node_role" {
@@ -68,26 +98,12 @@ resource "aws_iam_role" "eks_node_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_registry_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
+# Example of node group using the launch template
 resource "aws_eks_node_group" "superherogen-nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "superherogen-nodes" # Node group name
+  node_group_name = "superherogen-nodes"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = aws_subnet.eks_subnet[*].id
+  subnet_ids      = [aws_subnet.private_subnet.id]
 
   scaling_config {
     desired_size = 2
@@ -96,7 +112,7 @@ resource "aws_eks_node_group" "superherogen-nodes" {
   }
 
   launch_template {
-    id      = "lt-029da6ff0842ef8bd" # Your existing launch template ID
-    version = "$Latest"              # Use the latest version of the launch template
+    id      = "lt-0e738e8f6a66ba511" # Existing launch template ID
+    version = "$Latest"
   }
 }
